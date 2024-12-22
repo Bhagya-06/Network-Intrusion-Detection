a2967e8775b3c3d7538c8f0b13d0cc3b734563bb
Bhagya B <114055494+Bhagya-06@users.noreply.github.com>
2024-12-22 20:09:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# Install Java
-apt-get update && apt-get install -y openjdk-11-jre-headless && apt-get clean
+# Update and install Java
+apt-get update
+apt-get install -y openjdk-11-jre-headless
+apt-get clean
